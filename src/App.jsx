ea10ffc589c018bfc502deca91fc79a8d6cad153
Kaveenxz <kaveenhansithx@gmail.com>
2024-05-08 21:30:22 +0530

    make the structure in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experence from './components/experence/Experence'
+import Portfolio from './components/portfolio/Portfolio'
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
 
 const App = () => {
   return (
     <>
         <Header/>
+        <Nav/>
+        <About/>
+        <Experence/>
+        <Portfolio/>
+        <Testimonials/>
+        <Contact/>
+        <Footer/>
     </>
   )
 }
