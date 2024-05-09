9045aa2567dd82d896177f121740734f2ce6a95e
Kaveenxz <kaveenhansithx@gmail.com>
2024-05-09 12:52:12 +0530

    style heder section

--- src/components/header/HeaderSocial.jsx
+++ src/components/header/HeaderSocial.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
-import {FaGitHub} from 'react-icons/bs'
-import {Fidribble} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
+import {FiDribbble} from 'react-icons/fi'
 
 function HeaderSocial() {
   return (
     <div className='header__socials'>
         <a href="https://www.linkedin.com/in/kaveen-hansith-a4a484283/" target='_blank'><BsLinkedin/></a>
-        <a href="https://github.com/Kaveenxz" target='_blank'><FaGitHub/></a>
-        <a href="https://dribbble.com/KaveenHansith"><Fidribble/></a>
+        <a href="https://github.com/Kaveenxz" target='_blank'><FaGithub/></a>
+        <a href="https://dribbble.com/KaveenHansith"><FiDribbble/></a>
     </div>
   )
 }
