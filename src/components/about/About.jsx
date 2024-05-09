a992e70951a6fbc6a1d9e2c3587fbadb0dd64404
Kaveenxz <kaveenhansithx@gmail.com>
2024-05-09 15:28:18 +0530

    finish styling about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -21,7 +21,7 @@ const About = () => {
           <article className="about__card">
             <FaAward className='about__icon'/>
             <h5>Experences</h5>
-            <small>1+ Years Experences</small>
+            <small>1+ Years Working</small>
           </article>
 
           <article className="about__card">
