0dfb81b17f55b46b8fce7d8286ccb89e746a3d48
Kaveenxz <kaveenhansithx@gmail.com>
2024-05-09 15:00:52 +0530

    complete about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,50 @@
 import  './about.css'
-
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
+import myImg from '../../assests/my2.jpeg'
 const About = () => {
   return (
-   <section id='about'></section>
+   <section id='about'>
+    <h5>Get to Know</h5>
+    <h2>About Me</h2>
+
+    <div className="container about__container">
+      <div className="about__me">
+        <div className="about__me-image">
+          <img src={myImg} alt="about img" />
+        </div>
+      </div>
+
+      <div className="about__content">
+        <div className="about__cards">
+          <article className="about__card">
+            <FaAward className='about__icon'/>
+            <h5>Experences</h5>
+            <small>1+ Years Experences</small>
+          </article>
+
+          <article className="about__card">
+            <FiUsers className='about__icon'/>
+            <h5>Clients</h5>
+            <small>100+ Worldwide</small>
+          </article>
+
+          <article className="about__card">
+            <VscFolderLibrary className='about__icon'/>
+            <h5>Projects</h5>
+            <small>20+ Projects</small>
+          </article>
+        </div>
+
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero maiores veniam, provident nihil facilis laborum similique consequuntur corporis! Amet similique suscipit beatae consequatur nihil, optio tenetur maxime animi. Eum, expedita.
+        </p>
+
+        <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+      </div>
+    </div>
+   </section>
   )
 }
 
